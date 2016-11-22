90d8aa764fbfa40f9979a413dbabb69cf63896be
ywyz <admin@ywyz.tech>
2016-11-22 17:58:15 +0800

    Revert "fix 7.12.8.c ; add 7.12.7.c"
    This reverts commit 919ab96dcbf1ab06f3c00446e35796e9e916b3a1.

--- 第七章练习/7.12.8/7.12.8.c
+++ 第七章练习/7.12.8/7.12.8.c
@@ -11,7 +11,7 @@
 #define PER3 0.25
 void print(void);
 float hours(void);
-float rates(int money);
+void rate(int money);
 
 void print(void)
 {
@@ -38,7 +38,7 @@ float hours(void)
 	return hours;
 }
 
-float rates(int money)
+void rate(int money)
 {
 	int rates;
 	if (money <= RATE1)
@@ -47,41 +47,39 @@ float rates(int money)
 		rates = RATE1 * PER1 + (money - RATE1) * PER2;
 	else
 		rates = RATE1 * PER1 + 150 * PER2 + (money - RATE2) * PER3;
-	return rates;
+	printf("%f      %f       %f   ", money, rates, money - rates);
 }
 
 int main(void)
 {
-	int n, moneys;
-	float hour, rate;
+	int n, moneys, hour;
 
 	print();
-	while ((scanf("%d", &n)) == 1 && n != 5)
+	while ((n = getchar()) != 5)
 	{
 		hour = hours();
 		switch (n)
 		{
-		case 1 :
+		case '1':
 			moneys = hour * BASE1;
 			break;
-		case 2 :
+		case '2':
 			moneys = hour * BASE2;
 			break;
-		case 3 :
+		case '3':
 			moneys = hour * BASE3;
 			break;
-		case 4 :
+		case '4':
 			moneys = hour * BASE4;
 			break;
-		case '\n' :
+		case '\n':
 			continue;
 		default:
-			printf("Enter 1~5£º");
+			printf("Enter 1~5");
 			continue;
 		}
 
-		rate = rates(moneys);
-		printf(" %d    %f     %f\n", moneys, rate, moneys - rate);
+		void rate(moneys);
 		print();
 	}
 
