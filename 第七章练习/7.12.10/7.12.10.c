ec376ccda82c35fe1eca57e2507e5425b369d2fe
ywyz <admin@ywyz.tech>
2016-12-19 14:01:57 +0800

    update

--- 第七章练习/7.12.10/7.12.10.c
+++ 第七章练习/7.12.10/7.12.10.c
@@ -5,4 +5,65 @@
 #define BASE4 14875
 #define BASIC1 0.15
 #define BASIC2 0.28
+void rates(float money, int base);
+void display(void);
+float money(void);
+int main(void)
+{
+	float moneys;
+	int ch;
+	display();
+	while ((ch = getchar()) != '5')
+	{
+		switch (ch)
+		{
+			case '1':
+				moneys = money();
+				rates(moneys, BASE1);
+				break;
+			case '2':
+				moneys = money();
+				rates(moneys, BASE2);
+				break; 
+			case '3':
+				moneys = money();
+				rates(moneys, BASE3);
+				break;
+			case '4':
+					moneys = money();
+					rates(moneys, BASE3);
+					break;
+			case '\n':
+				continue;
+			default:
+				printf("Enter 1-5");
+				break;
+		}
+		display();
+	}
+	printf("Bye!\n");
+}
+
+void display(void)
+{
+	printf("(1)17850         (2)23900\n");
+	printf("(3)29750         (4)14875\n");
+}
+
+float money(void)
+{
+	float moneys;
+	scanf("%f", &moneys);
+	return moneys;
+}
+
+void rates(float money, int base)
+{
+	float rate;
+	if (money < base)
+		rate = money * BASIC1;
+	else
+		rate = (money - base) * BASIC2 + base * BASIC1;
+	printf("%f", rate);
+}
 
